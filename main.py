5dc0f483dfba70e60af410e7d126fd580171a876
Magdaléna Šimčíková <150794753+magdasimcik@users.noreply.github.com>
2024-04-17 14:28:41 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
+    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+
+def caesar(text, shift, direction):
+    end_text = ""
+    if direction == "decode":
+        shift *= -1
+    for char in text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {direction}d result: {end_text}")
+
+
+from art import logo
+
+print(logo)
+
+restart = True
+while restart:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift %= 26
+
+    caesar(text, shift, direction)
+    result = input(
+        "\nType 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if result == "no":
+        restart = False
+        print("Goodbye")
